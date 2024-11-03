98eb8f9a4df28881246aa5440963fbde26d68c40
ice breaker <1324318532@qq.com>
2024-11-03 15:40:12 +0800

    chore: 添加独立分包使用 vue-mini 的示例

--- src/package/pages/index/index.ts
+++ src/package/pages/index/index.ts
@@ -4,18 +4,14 @@ defineComponent(() => {
   const greeting = ref('欢迎使用 Vue Mini')
   const bg = ref('bg-gradient-to-r from-[#456789] to-[#987654] h-[123.456px] text-[#fafafa] flex items-center justify-center')
   const count = ref(0)
-  function go2subpackgae() {
+  function addCount() {
     count.value++
-    console.log('go2subpackgae')
-    wx.navigateTo({
-      url: '/package/pages/index/index',
-    })
   }
 
   return {
     count,
     greeting,
     bg,
-    go2subpackgae,
+    addCount,
   }
 })
