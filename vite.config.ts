f35834a8ae76b5530087fad9eece3813dcba0f91
ice breaker <1324318532@qq.com>
2025-05-02 23:25:38 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
+import { TDesignResolver } from 'weapp-vite/auto-import-components/resolvers'
 import { defineConfig } from 'weapp-vite/config'
 
 export default defineConfig({
   weapp: {
     srcRoot: './src',
+    enhance: {
+      autoImportComponents: {
+        resolvers: [TDesignResolver()],
+      },
+    },
   },
   plugins: [
     UnifiedViteWeappTailwindcssPlugin({
